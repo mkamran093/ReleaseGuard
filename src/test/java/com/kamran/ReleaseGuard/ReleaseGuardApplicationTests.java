e3056570300b40616f4168930d11c3315ae3a23f
Muhammad Kamran <mkamran.bscssef20@iba-suk.edu.pk>
2026-09-24 00:26:05 +0500

    Created basic project structure

---
+++ src/test/java/com/kamran/ReleaseGuard/ReleaseGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamran.ReleaseGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
